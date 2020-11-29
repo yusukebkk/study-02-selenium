78997de8bbefa18c81ea3efd5d9696394bb9e25f
marutoraman <33813153+marutoraman@users.noreply.github.com>
2020-11-29 16:40:35 +0900

    ポップアップがない場合でもエラーにならないようにする
    ポップアップがない場合でもエラーにならないようにする

--- mynavi_sample.py
+++ mynavi_sample.py
@@ -38,12 +38,16 @@ def main():
     # Webサイトを開く
     driver.get("https://tenshoku.mynavi.jp/")
     time.sleep(5)
-    # ポップアップを閉じる
-    driver.execute_script('document.querySelector(".karte-close").click()')
-    time.sleep(5)
-    # ポップアップを閉じる
-    driver.execute_script('document.querySelector(".karte-close").click()')
-
+ 
+    try:
+        # ポップアップを閉じる
+        driver.execute_script('document.querySelector(".karte-close").click()')
+        time.sleep(5)
+        # ポップアップを閉じる
+        driver.execute_script('document.querySelector(".karte-close").click()')
+    exception:
+        pass
+    
     # 検索窓に入力
     driver.find_element_by_class_name(
         "topSearch__text").send_keys(search_keyword)
